c02f8eeb84d3883727d1f584d9b2b82760eb30e6
lishhPhper <nietzsche_nc@163.com>
2019-09-11 18:13:50 +0800

    测试微信公众号授权

--- application/api/controller/Test.php
+++ application/api/controller/Test.php
@@ -12,8 +12,8 @@ class Test extends Controller
     {
         $appid = 'wx9d1865ea3c06a468';
         $appsecret = '72b6ab9ddfff4641e0c9e8a356a6f06d';
-        $redirect_uri = urlencode('http://flower.wlforever.com/api/test/getUserInfo');
-        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_base&state=111#wechat_redirect';
+        $redirect_uri = urlencode('http://flower.wlforever.com/api/test/getUserOpenId');
+        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=111#wechat_redirect';
         header("location:".$url);
     }
 
@@ -23,7 +23,12 @@ class Test extends Controller
         $appsecret = '72b6ab9ddfff4641e0c9e8a356a6f06d';
         $code = $this->request->param('code');
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$appsecret.'&code='.$code.'&grant_type=authorization_code';
-        $res = Http::get($url);
-        dump($res);die;
+        $res = json_decode(Http::get($url),true);
+        $access_token = $res['access_token'];
+        $open_id = $res['openid'];
+
+        $url2 = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$open_id.'&lang=zh_CN';
+        $userInfo = Http::get($url2);
+        dump($userInfo);die;
     }
 }
